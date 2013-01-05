e173b04f9b4836129726514bfefc7de6e18c98ab
Marcos Vilela <vilelam@gmail.com>
2013-01-05 18:03:09 -0200

    Initial Commit

---
+++ InventoryManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InventoryManager
+//
+//  Created by Marcos Vilela on 05/01/13.
+//  Copyright (c) 2013 Marcos Vilela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InventoryManager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InventoryManager
+//
+//  Created by Marcos Vilela on 05/01/13.
+//  Copyright (c) 2013 Marcos Vilela. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
